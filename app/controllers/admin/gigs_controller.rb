2852bd01f757c6fa8355acf16fadf94a6203e475
Cory Hack <cory.hack@gmail.com>
2019-04-17 14:50:37 -0400

    Add Gig show, new and create functionality

--- app/controllers/admin/gigs_controller.rb
+++ app/controllers/admin/gigs_controller.rb
@@ -1,5 +1,23 @@
 class Admin::GigsController < ApplicationController
+  before_action :authenticate_user!
   def new
-    
+    @gig = Gig.new
   end
+
+  def create
+    @gig = current_user.gigs.create(gig_params)
+    redirect_to admin_gig_path(@gig)
+  end
+
+  def show
+    @gig = Gig.find(params[:id])
+  end
+
+  private
+
+  def gig_params
+    params.require(:gig).permit(:title, :description)
+  end
+
+
 end
