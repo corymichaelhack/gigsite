0742d8909857a6a5868a26d9939d34c29265b85e
Cory Hack <cory.hack@gmail.com>
2019-06-03 16:57:06 -0400

    Add validation for Notes with display error handling

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,14 +2,30 @@ class NotesController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_site, only: [:create, :new]
 
+  def index
+    
+  end
+
   def new
     @site = Site.find(params[:site_id])
     @note = Note.new
   end
 
   def create
-    @site = current_site.notes.create(note_params)
-    redirect_to gig_site_path(current_site.gig, current_site)
+    @site = Site.find(params[:site_id])
+
+    @note = current_site.notes.create(note_params)
+
+    respond_to do |format|
+      if @note.save
+        format.html { redirect_to gig_site_path(current_site.gig, current_site), notice: 'Note was successfully created.' }
+      else
+        format.html { render :new}
+      end
+    end
+
+
+    # redirect_to gig_site_path(current_site.gig, current_site)
   end
 
   private
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,11 @@
 class Note < ApplicationRecord
   belongs_to :site
+  validates :body, presence: { 
+                    message: "Note can not be blank"},length: {
+                        minimum: 2, 
+                          too_short: "%{count} characters is the minimum allowed", 
+                          maximum: 1000, 
+                            too_long: "%{count} characters is the maximum allowed" }
 
   mount_uploader :image, ImageUploader
 end
